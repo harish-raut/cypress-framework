ac41f6393229da37595e246a232f899a50cb4738
Harish <hraut@qamentor.com>
2023-11-09 11:22:04 +0530

    fixed github workflow 2

---
+++ devOps/cypress-ci-run.ts
@@ -0,0 +1,55 @@
+import { exec } from 'child_process';
+
+type GetEnvOptions = {
+    required?: boolean;
+};
+
+function getEnvNumber(varName: string, { required = false }: GetEnvOptions = {}): number {
+    if (required && process.env[varName] === undefined) {
+        throw Error(`${varName} is not set.`);
+    }
+
+    const value = Number(process.env[varName]);
+
+    if (isNaN(value)) {
+        throw Error(`${varName} is not a number.`);
+    }
+
+    return value;
+}
+
+function getArgs() {
+    return {
+        totalRunners: getEnvNumber('TOTAL_RUNNERS', { required: true }),
+        thisRunner: getEnvNumber('THIS_RUNNER', { required: true }),
+    };
+}
+
+(async () => {
+    try {
+        const { totalRunners, thisRunner } = getArgs();
+
+        const command = `npx cypress run --spec "$(npx --silent ts-node --quiet devOps/cypress-spec-split.ts ${totalRunners} ${thisRunner})"`;
+
+        console.log(`Running: ${command}`);
+
+        const commandProcess = exec(command);
+
+        // pipe output because we want to see the results of the run
+
+        if (commandProcess.stdout) {
+            commandProcess.stdout.pipe(process.stdout);
+        }
+
+        if (commandProcess.stderr) {
+            commandProcess.stderr.pipe(process.stderr);
+        }
+
+        commandProcess.on('exit', (code) => {
+            process.exit(code || 0);
+        });
+    } catch (err) {
+        console.error(err);
+        process.exit(1);
+    }
+})();
